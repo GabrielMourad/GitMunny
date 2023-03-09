31aa255e6203119ad362574b8a3e1f88a1b69c9f
GabrielMourad <gabemrad@yahoo.com>
2023-03-09 14:50:16 -0800

    updated login

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -12,25 +12,28 @@ const LoginPage = () => {
 
   return (
     <div class="container-login">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <div class="login-form">
-                    <h2>Login</h2>
-                    <form>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter email"/>
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Password"/>
-                        </div>
-                        <button type="submit" class="btn btn-primary-login">Login</button>
-                    </form>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="login-form">
+                <h2>Login</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Enter email"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Password"/>
+                    </div>
+                    <button type="submit" class="btn btn-primary-login">Login</button>
+                </form>
+                <div class="signup-section">
+                    <p>Don't have an account yet? <a href="#">Sign Up</a></p>
                 </div>
             </div>
         </div>
     </div>
+</div>
   );
 }
 
