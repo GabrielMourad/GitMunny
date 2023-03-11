696aec71f852d82272507ebaa49de5e576058df7
GabrielMourad <gabemrad@yahoo.com>
2023-03-11 15:26:43 -0800

    reverting back

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { googleSignIn } from "../firebase/Firebase.js"
 
 
 
--- client/src/components/modals/TransactionModal.js
+++ client/src/components/modals/TransactionModal.js
@@ -9,25 +9,27 @@ export default function BudgetModal(props) {
   
   return (
     <>
-     
+  
 
-      <div class="modal fade modal-lg" id="transaction-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog text-dark">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Expense</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-        <div class="modal-body">
-          <AddExpenseForm/>
-        </div>
-          <div class="modal-footer">
-            <button id = "close-modal" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button class="btn btn-primary" form = "expense-form" type = "submit"  >Save</button>
+        <div class="modal fade modal-lg" id="transaction-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-scrollable text-dark">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Expense</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
+              <div class="modal-body">
+                <AddExpenseForm/>
+              </div>
+              <div class="modal-footer">
+                <button id="close-modal" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button class="btn btn-primary" form="expense-form" type="submit">Save</button>
+              </div>
+            </div>
           </div>
-        </div>
       </div>
+
+    
     </>
  
   )
