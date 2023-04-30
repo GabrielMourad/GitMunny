c60f13f2c83c96cec3a1336602c8ebeba71e04bf
GabrielMourad <gabemrad@yahoo.com>
2023-04-30 16:23:56 -0700

    starting to implement leaderboard design

---
+++ client/src/components/Leaderboard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Leaderboard() {
+  return (
+    <>
+    <div className="container">
+      <p className = "lboard_title">Leaderboard</p>
+
+      <table className = "lboard">
+
+        <tbody>
+          <tr>
+            <th>Rank</th>
+            <th>User</th>
+            <th>Score</th>
+          </tr>
+
+          <tr>
+            <td>mommy</td>
+            <td>mommy</td>
+            <td>mommy</td>
+          </tr>
+        </tbody>
+      </table>
+
+    </div>
+    </>
+  )
+}
