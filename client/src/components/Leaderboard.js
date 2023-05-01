0aab19375caa390210f54561ce39359de40cb478
GabrielMourad <gabemrad@yahoo.com>
2023-04-30 21:02:28 -0700

    leaderboard

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
 export default function Leaderboard() {
+  const leaderboard = [
+    { rank: 1, user: "Alice", score: 100 },
+    { rank: 2, user: "Bob", score: 90 },
+    { rank: 3, user: "Charlie", score: 80 },
+    { rank: 4, user: "Dave", score: 70 },
+    { rank: 5, user: "Eve", score: 60 },
+    { rank: 6, user: "Frank", score: 50 },
+    { rank: 7, user: "Grace", score: 40 },
+    { rank: 8, user: "Harry", score: 30 },
+    { rank: 9, user: "Ivy", score: 20 },
+    { rank: 10, user: "John", score: 10 }
+  ];
+  
   return (
     <>
     <div className="container">
-      <p className = "lboard_title">Leaderboard</p>
+      <div className = "lboard_title">Leaderboard</div>
 
       <table className = "lboard">
 
@@ -15,11 +28,23 @@ export default function Leaderboard() {
             <th>Score</th>
           </tr>
 
-          <tr>
+          {leaderboard.map((obj) => (
+              <>
+                <tr>
+                  <td>{obj.rank}</td>
+                  <td>{obj.user}</td>
+                  <td>{obj.score}</td>
+
+                </tr>
+
+              </>
+            ))}
+
+          {/* <tr>
             <td>mommy</td>
             <td>mommy</td>
             <td>mommy</td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
 
